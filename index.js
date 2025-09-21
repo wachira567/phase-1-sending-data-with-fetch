e6faa9a3170b83e9d6c8b6b5c6d19c0332c69996
+ <+>
2025-09-21 11:29:04 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  const user = {
+    name: name,
+    email: email,
+  };
+
+  const settings = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+
+  return fetch("http://localhost:3000/users", settings)
+    .then((res) => res.json())
+    .then((data) => {
+      document.body.innerHTML = data.id;
+    })
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
+}
